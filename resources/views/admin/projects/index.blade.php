97d790ab124881ba6d49fe82af7041a2fac478d7
riccardotrezzi <riccardo.trezzi00@gmail.com>
2024-10-30 18:12:43 +0100

    migration e seeder a posto creato l'index, ma ho un problema con la fase di route

---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('page-title', 'Progetti')
+
+@section('main-content')
+    <div class="row">
+        <div class="col">
+            <div class="card">
+                <div class="card-body">
+                    <h1 class="text-center text-success">
+                        Guarda tutti i miei progetti!
+                    </h1>
+                </div>
+
+                
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 // Controllers
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\Admin\MainController as AdminMainController;
+use App\Http\Controllers\Admin\ProjectController as AdminProjectController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,6 +26,8 @@ Route::prefix('admin')
     ->group(function () {
 
     Route::get('/dashboard', [AdminMainController::class, 'dashboard'])->name('dashboard');
+
+    Route::resource('projects', AdminProjectController::class);
 
 });
 
