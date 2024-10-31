d62f67b1bd92ab692fec90964fe09f7ae22d1d06
riccardotrezzi <riccardo.trezzi00@gmail.com>
2024-10-31 08:20:46 +0100

    sistemato errore, namespace non aggiornato in ProjectController

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::truncate();
+        
         User::factory()->create([
             'name' => 'Riccardo Trezzi',
             'email' => 'riccardo@trezzi.com',
